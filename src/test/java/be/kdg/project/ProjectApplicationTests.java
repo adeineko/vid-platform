c9900d1ccee8bb7a034bd2e308a3d4af0d8cd20e
anna.deineko <anna.deineko@student.kdg.be>
2023-11-03 13:04:17 +0100

    New

---
+++ src/test/java/be/kdg/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.kdg.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
